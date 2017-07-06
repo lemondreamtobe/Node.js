61356698bd169a084814af11f16413047e674b3e
lemondreamtobe <897418436@qq.com>
2017-07-06 17:52:53 +0800

    yp 2

--- app/callback.js
+++ app/callback.js
@@ -2,4 +2,17 @@
  * 回调和异步回调区别
  * 
  */
-function
+function waitFive(name, function_name) {
+	var pus = 0;
+	var currentDate = new Date();
+	while(pus < 5000) {
+		var now = new Date();
+		pus = now - currentDate;
+	};
+	function_name(name);
+};
+function echo(name) {
+	console.log(name);
+};
+waitFive('feng', echo);
+console.log('its over');
