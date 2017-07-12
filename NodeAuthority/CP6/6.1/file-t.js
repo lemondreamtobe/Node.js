98794f72336c4aa527cd772a5bb59c1fce07a93c
lemondreamtobe <897418436@qq.com>
2017-07-12 17:22:11 +0800

    //

--- NodeAuthority/CP6/6.1/file-t.js
+++ NodeAuthority/CP6/6.1/file-t.js
@@ -7,15 +7,15 @@ fs.readFile('./index.html', function (err, data) {
         console.log(data.toString())
     }
 });
-fs.appendFile('./test.text','hello world','utf-8', function (err, data) {
-
-    if (err) {
-        console.log('err');
-        return;
-    } else {
-        console.log('success');
-    }
-});
+// fs.appendFile('./test.text','hello world','utf-8', function (err, data) {
+//
+//     if (err) {
+//         console.log('err');
+//         return;
+//     } else {
+//         console.log('success');
+//     }
+// });
 fs.open('./test.text','r',function (err, fd) {
 
     if (err) {
