f461587d540a763a141aa1938a02c2e644ec0e95
lemondreamtobe <897418436@qq.com>
2017-07-12 17:12:22 +0800

    权威指南

--- studyNode/app/app.js
+++ studyNode/app/app.js
@@ -5,8 +5,9 @@
  */
 var http = require('http');
 http.createServer(function(req, res) {
-	var reHtml='<html><head><title>node i like u</title></head><body><div>hi, i like node.js</div></body></html>'
+	var reHtml='<html><head><meta charset="utf-8"><title>node i like u</title></head><body><div>hi, i like node.js</div></body></html>'
 	res.writeHead(200,{'Content-Type' : 'text/html'});
-	res.end(reHtml);
+	res.write(reHtml);
+	res.end('你好');
 }).listen(2000, '127.0.0.1');
 console.log('sever runing at http://127.0.0.1:2000');
